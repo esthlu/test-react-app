d06ad89565acfcab6f0abe079ebb666b466eb6f7
Esther Lu <eeyan14@gmail.com>
2020-04-02 20:57:12 -0400

    Updates

---
+++ precache-manifest.431d6b1c850e32afb5510310acb4d944.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d7f5f5ae6ed44f3a7da8799dd1f0b24",
+    "url": "/test-react-app/index.html"
+  },
+  {
+    "revision": "21af19125f18b4986dfe",
+    "url": "/test-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bb76f283a0ac18db1ad0",
+    "url": "/test-react-app/static/js/2.75bfb383.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-react-app/static/js/2.75bfb383.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21af19125f18b4986dfe",
+    "url": "/test-react-app/static/js/main.9aea7a16.chunk.js"
+  },
+  {
+    "revision": "5784607fde75f389fb21",
+    "url": "/test-react-app/static/js/runtime-main.138d57ca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
